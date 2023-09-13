2cae618a888989236a55840fdbceaedf63144a2e
Martin Casey <martinc140291@gmail.com>
2023-09-14 08:38:27 +1000

    Completed JavaScript

--- script.js
+++ script.js
@@ -1,57 +1,42 @@
-// Capture the Form Submission button in a variable
-const btn = document.querySelector(".submit-button");
-
-
-// Event Listener for the Form Submission Event
-btn.addEventListener("click", formSubmission);
-
+const btn = document.querySelector('#submit-button');
+const output = document.querySelector('#output');
+const radioButtons = document.querySelectorAll('input[name="feedback"]');
 //Initiate a variable for the main div, to be used below
 const mainContainer = document.getElementById('main-container');
 
-// Initiate variable for feedback rating value
-let feedbackRating = 0;
+btn.addEventListener("click", () => {
+  let selectedAmount;
+  for (const radioButton of radioButtons) {
+      if (radioButton.checked) {
+        selectedAmount = radioButton.value;
+        break;
+      }
+  }
 
-// Function for the Form Submission Event
-function formSubmission() {
-  // see if a radio button is checked
-feedbackRating = document.querySelector('input[name="feedback"]:checked').value;
-//if a button is checked the value will be above 0
-if (feedbackRating > 0) {
-  formSuccess(feedbackRating);
-} else {
-  formFail();
-}
-}
+selectedAmount ? formSuccess(selectedAmount) : formFail();
 
-// Form success function - i.e. an actual rating has been selected and submitted
+//output.innerText = selectedAmount ? `You selected ${selectedAmount}` : `You haven't selected any size`;
+});
+
 function formSuccess(rating) {
-  // Variable to count number of Radio Buttons
-  const radioButtons = document.querySelectorAll('input[name="feedback"]').length;
-  
-  //Remove elements that we no longer need
+//Remove elements that we no longer need
   let headingElement = document.getElementById('main-heading');
   headingElement.classList.remove("main-heading");
   let formElement = document.getElementsByClassName("rating-form");
   formElement[0].remove();
 
- 
-
-     
-
-
-
-  //Add a new heading in small orange text with the rating added dynamically
+//Add a new heading in small orange text with the rating added dynamically
   headingElement.classList.add("main-heading-alternate");
-  headingElement.innerHTML = `You selected ${rating} out of ${radioButtons}`;
+  headingElement.innerHTML = `You selected ${rating} out of ${radioButtons.length}`;
 
 // Add a larger 'thank you' subheading
 const newHeading = document.createElement("h2");
- // Give it some content
- newHeading.innerHTML = `Thank you!`;
+// Give it some content
+newHeading.innerHTML = `Thank you!`;
 // Give it the right class
 newHeading.classList.add("sub-heading-alternate");
 // Add it to the div
- mainContainer.appendChild(newHeading);
+mainContainer.appendChild(newHeading);
 
 //Add a paragraph with a message of appreciation
 const newPara = document.createElement("p");
@@ -63,4 +48,23 @@ newPara.classList.add('rating-description-alternate');
 mainContainer.appendChild(newPara);
 }
 
+function formFail() {
+    //Create paragraph for error message
+  let errorMessage = document.createElement("p");
+   // Give it some content
+   errorMessage.innerHTML = `Please input a rating before you submit`;
+   // Give it some styling
+   errorMessage.classList.add('error-message');
+   //Give it an ID so we can target it with the set timeout function
+   errorMessage.setAttribute("id", "errorMessage")
+   // Add it to the div
+   mainContainer.appendChild(errorMessage);
+   //Remove the text after 10 seconds
+   setTimeout(() => {
+      const errorRemove = document.getElementById('errorMessage');
+
+      //remove element from DOM
+      errorRemove.style.display = 'none';
+   }, 10000);
+}
 
